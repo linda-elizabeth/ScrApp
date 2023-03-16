c9d42b41e4c9973b2d55648162d1494ed926756e
linda-elizabeth <lindaliz5995@gmail.com>
2023-03-16 18:31:41 +0530

    require dotenv

--- src/models/SearchData.js
+++ src/models/SearchData.js
@@ -8,6 +8,12 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.connection.on("error", (err) => {
+  console.log("err", err);
+});
+mongoose.connection.on("connected", (err, res) => {
+  console.log("mongoose is connected");
+});
 //schema definition
 const Schema = mongoose.Schema;
 //constructor
